361cb1cc478b4151d3c6f8a41cd537f5aecec7e8
Muhammad Rizwan Niazi <muhammad.rizwan@groupdocs.com>
2017-10-14 04:26:26 +0500

    Added Visual Studio AddIn - VSPlugin

---
+++ Plugins/GroupDocs_Editor_VSPlugin/GroupDocs.Editor.VisualStudioPlugin/GroupDocsVisualStudioPlugin/Core/GroupDocsComponents.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Aspose 2002-2016. All Rights Reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace GroupDocsEditorVisualStudioPlugin.Core
+{
+    public class GroupDocsComponents
+    {
+        public static Dictionary<String, GroupDocsComponent> list = new Dictionary<string, GroupDocsComponent>();
+        public GroupDocsComponents()
+        {
+            list.Clear();
+
+            GroupDocsComponent groupdocsEditor = new GroupDocsComponent();
+            groupdocsEditor.set_downloadUrl("");
+            groupdocsEditor.set_downloadFileName("groupdocs.Editor.zip");
+            groupdocsEditor.set_name(Constants.GROUPDOCS_COMPONENT);
+            groupdocsEditor.set_remoteExamplesRepository("https://github.com/groupdocs-Editor/GroupDocs.Editor-for-.NET.git");
+            list.Add(Constants.GROUPDOCS_COMPONENT, groupdocsEditor);
+        }
+    }
+}
