be3f5c0210445cced9871afee966f236d1e35d5a
Yevgen Nykytenko <Yevgen.Nykytenko@aspose.com>
2021-06-04 13:15:06 +0300

    Added MVC project

---
+++ Demos/MVC/GroupDocs.Editor.MVC.Test/EditorControllerTest.cs
@@ -0,0 +1,80 @@
+﻿using GroupDocs.Editor.MVC.Controllers;
+using NUnit.Framework;
+using System.Web.Routing;
+using MvcContrib.TestHelper;
+using Huygens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using GroupDocs.Editor.MVC.Products.Common.Entity.Web;
+
+namespace GroupDocs.Editor.MVC.Test
+{
+    [TestFixture]
+    public static class EditorControllerTest {
+       
+
+        [SetUp]
+        public static void TestInitialize()
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        [TearDown]
+        public static void TearDown()
+        {
+            RouteTable.Routes.Clear();
+        }
+
+        [Test]
+        public static void ViewStatusTest()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "/../../../src";
+            using (var server = new DirectServer(path))
+            {
+                var request = new SerialisableRequest
+                {
+                    Method = "GET",
+                    RequestUri = "/editor",
+                    Content = null
+                };
+
+                var result = server.DirectCall(request);
+                Assert.That(result.StatusCode, Is.EqualTo(200));
+            }
+        }
+
+        [Test]
+        public static void ViewMapControllerTest()
+        {
+            "~/editor".Route().ShouldMapTo<EditorController>(x => x.Index());
+        }
+
+        [Test]
+        public static void FileTreeStatusCodeTest()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "/../../../src";
+            using (var server = new DirectServer(path))
+            {
+
+                PostedDataEntity requestData = new PostedDataEntity();
+                requestData.path = "";
+
+                var request = new SerialisableRequest
+                {
+                    Method = "POST",
+                    RequestUri = "/loadfiletree",
+                    Content = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(requestData)),
+                    Headers = new Dictionary<string, string>{
+                        { "Content-Type", "application/json"},
+                        { "Content-Length", JsonConvert.SerializeObject(requestData).Length.ToString()}
+                    }
+                };
+
+                var result = server.DirectCall(request);
+                Assert.That(result.StatusCode, Is.EqualTo(200));
+            }
+        }        
+    }
+}
