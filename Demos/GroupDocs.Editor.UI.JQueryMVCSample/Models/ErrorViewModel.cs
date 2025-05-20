1819aed54cdeb196971411363ef49da331f22b5d
Viktor Stupak <viktor.stupak@aspose.com>
2025-05-20 09:18:00 +0200

    Updated GroupDocs.Editor for .NET version to 25.05

--- Demos/GroupDocs.Editor.UI.JQueryMVCSample/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GroupDocs.Editor.UI.JQueryMVCSample.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
